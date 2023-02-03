3000ee1785d5bdcf47009491c298e1e860422790
hard.tender.blade <matsenkodenis0@gmail.com>
2023-02-03 17:39:39 +0100

    Update login page

--- rest-timer/src/components/sup-pages/Login.jsx
+++ rest-timer/src/components/sup-pages/Login.jsx
@@ -3,15 +3,22 @@ import React, { useState }from "react";
 export default function Login() {
    return (
      <div className="Login bg-red-200 h-screen w-screen flex justify-center items-center">
-        <div className="Contents bg-slate-400 h-96 w-96">
-            <div className="text-3xl font-bold">
+        <div className="Contents bg-slate-400 h-96 w-96 flex flex-col">
+            <div className="text-3xl font-bold text-center mt-4">
                 Login to continue
             </div>
-            <input type="login"/>
-            <input type="password"/>
+            <input className="mt-6" type="login"/>
+            <input className="mt-6" type="password"/>
 
-            <button>Log in</button>
-            <button>Sign up</button>
+            <div className="flex justify-between">
+                <button>Log in</button>
+                <button>Sign up</button>
+            </div>
+
+            <div className="text-end">Forgotten password</div>
+
+            <button>Content with Google</button>
+
         </div>
         
      </div>  
