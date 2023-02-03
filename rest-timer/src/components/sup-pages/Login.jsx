9eb6da185614b12e9177bf6d5e5c59c042aa6657
hard.tender.blade <matsenkodenis0@gmail.com>
2023-02-03 17:12:20 +0100

    Update Login page

--- rest-timer/src/components/sup-pages/Login.jsx
+++ rest-timer/src/components/sup-pages/Login.jsx
@@ -2,8 +2,18 @@ import React, { useState }from "react";
 
 export default function Login() {
    return (
-     <div className="Login">
-        <input type="text" />
+     <div className="Login bg-red-200 h-screen w-screen flex justify-center items-center">
+        <div className="Contents bg-slate-400 h-96 w-96">
+            <div className="text-3xl font-bold">
+                Login to continue
+            </div>
+            <input type="login"/>
+            <input type="password"/>
+
+            <button>Log in</button>
+            <button>Sign up</button>
+        </div>
+        
      </div>  
    );
 }
