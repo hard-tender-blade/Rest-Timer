710690665967abaa3d394d2d459e4be05a5e3c00
reixxe <m4ty1111@gmail.com>
2023-02-03 14:46:03 +0100

    Comment

---
+++ rest-timer/src/components/components/NavBar.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Pages } from "../../logic/Global";
+
+export default function NavBar(props) {
+  const [currentPage, setCurrentPage] = useState(Pages.Home);
+
+  //TODO chage code here
+  // kys
+  return (
+    <div className="w-screen bg-white">
+      <div className="max-w-7xl m-auto h-20 flex justify-around text-base text-grey-t font-intertight">
+        <button
+          className={currentPage === Pages.Home ? "font-inter font-bold text-text-b" : ""}
+          onClick={() => {
+            props.setCurrentPage(Pages.Home);
+            setCurrentPage(Pages.Home);
+          }}
+        >
+          Home
+        </button>
+        <button
+          className={currentPage === Pages.Medals ? "font-inter font-bold text-text-b" : ""}
+          onClick={() => {
+            props.setCurrentPage(Pages.Medals);
+            setCurrentPage(Pages.Medals);
+          }}
+        >
+          Medals
+        </button>
+        <button
+          className={currentPage === Pages.HealthInfo ? "font-inter font-bold text-text-b" : ""}
+          onClick={() => {
+            props.setCurrentPage(Pages.HealthInfo);
+            setCurrentPage(Pages.HealthInfo);
+          }}
+        >
+          Health info
+        </button>
+        <button
+          className={currentPage === Pages.Profile ? "font-inter font-bold text-text-b" : ""}
+          onClick={() => {
+            props.setCurrentPage(Pages.Profile);
+            setCurrentPage(Pages.Profile);
+          }}
+        >
+          Profile
+        </button>
+        <div className="w-96"></div>
+        <button
+          className={currentPage === Pages.About ? "font-inter font-bold text-text-b" : ""}
+          onClick={() => {
+            props.setCurrentPage(Pages.About);
+            setCurrentPage(Pages.About);
+          }}
+        >
+          About
+        </button>
+      </div>
+    </div>
+  );
+}
