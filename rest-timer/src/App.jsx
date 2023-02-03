06173935eb8b081afcb06d3cf6f7448bb4a231fc
hard.tender.blade <matsenkodenis0@gmail.com>
2023-02-03 17:51:13 +0100

    Last denis branch commit

--- rest-timer/src/App.jsx
+++ rest-timer/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import NavBar from "./components/components/NavBar";
 import { Pages } from "./logic/Global";
 
+
+//last denis commit
 export default function App() {
   const [CurrentPage, setCurrentPage] = useState(Pages.Home)
 
