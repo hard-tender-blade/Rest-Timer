41910281239f69002bbabd4537f433ac5ff07d7b
reixxe <m4ty1111@gmail.com>
2023-02-04 18:12:34 +0100

    Fixed navbar spacing issues

--- rest-timer/src/components/components/NavBar.jsx
+++ rest-timer/src/components/components/NavBar.jsx
@@ -8,43 +8,52 @@ export default function NavBar(props) {
   return (
     <div className="w-screen bg-white">
       <div className="max-w-7xl m-auto h-20 flex justify-around text-base text-grey-t font-intertight">
-        <button
-          className={currentPage === Pages.Home ? "font-inter font-bold text-text-b" : ""}
-          onClick={() => {
-            props.setCurrentPage(Pages.Home);
-            setCurrentPage(Pages.Home);
-          }}
-        >
-          Home
-        </button>
-        <button
-          className={currentPage === Pages.Medals ? "font-inter font-bold text-text-b" : ""}
-          onClick={() => {
-            props.setCurrentPage(Pages.Medals);
-            setCurrentPage(Pages.Medals);
-          }}
-        >
-          Medals
-        </button>
-        <button
-          className={currentPage === Pages.HealthInfo ? "font-inter font-bold text-text-b" : ""}
-          onClick={() => {
-            props.setCurrentPage(Pages.HealthInfo);
-            setCurrentPage(Pages.HealthInfo);
-          }}
-        >
-          Health info
-        </button>
-        <button
-          className={currentPage === Pages.Profile ? "font-inter font-bold text-text-b" : ""}
-          onClick={() => {
-            props.setCurrentPage(Pages.Profile);
-            setCurrentPage(Pages.Profile);
-          }}
-        >
-          Profile
-        </button>
+        <div className="w-24 m-auto text-center">
+          <button
+            className={currentPage === Pages.Home ? "font-inter font-bold text-text-b" : ""}
+            onClick={() => {
+              props.setCurrentPage(Pages.Home);
+              setCurrentPage(Pages.Home);
+            }}
+          >
+            Home
+          </button>
+        </div>
+        <div className="w-24 m-auto text-center">
+          <button
+            className={currentPage === Pages.Medals ? "font-inter font-bold text-text-b" : ""}
+            onClick={() => {
+              props.setCurrentPage(Pages.Medals);
+              setCurrentPage(Pages.Medals);
+            }}
+          >
+            Medals
+          </button>
+        </div>
+        <div className="w-24 m-auto text-center">
+          <button
+            className={currentPage === Pages.HealthInfo ? "font-inter font-bold text-text-b" : ""}
+            onClick={() => {
+              props.setCurrentPage(Pages.HealthInfo);
+              setCurrentPage(Pages.HealthInfo);
+            }}
+          >
+            Health info
+          </button>
+        </div>
+        <div className="w-24 m-auto text-center">
+          <button
+            className={currentPage === Pages.Profile ? "font-inter font-bold text-text-b" : ""}
+            onClick={() => {
+              props.setCurrentPage(Pages.Profile);
+              setCurrentPage(Pages.Profile);
+            }}
+          >
+            Profile
+          </button>
+        </div>
         <div className="w-96"></div>
+        <div className="w-24 m-auto text-center">
         <button
           className={currentPage === Pages.About ? "font-inter font-bold text-text-b" : ""}
           onClick={() => {
@@ -54,6 +63,7 @@ export default function NavBar(props) {
         >
           About
         </button>
+        </div>
       </div>
     </div>
   );
