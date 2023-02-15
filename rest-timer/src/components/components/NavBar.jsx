d7cead66a6432e3b966ec82e41a53993ea6b396e
hard.tender.blade <matsenkodenis0@gmail.com>
2023-02-15 08:24:56 +0100

    NavBar optimized

--- rest-timer/src/components/components/NavBar.jsx
+++ rest-timer/src/components/components/NavBar.jsx
@@ -4,112 +4,44 @@ import { Pages } from "../../logic/Global";
 export default function NavBar(props) {
   const [currentPage, setCurrentPage] = useState(Pages.Home);
 
-  //TODO chage code here
+  const NavBarBtnClick = (Page) => {
+    props.setCurrentPage(Page);
+    setCurrentPage(Page);
+  }
+
+  const GetButtonName = (Page) => {
+    for (const [key, value] of Object.entries(Pages)) {
+      if(value === Page) return key
+    }
+  }
+
+  const NavBarBtn = (Page) => {
+    return (
+      <>
+        <div className="w-24 m-auto text-center">
+
+          <button
+            className={`${currentPage === Page ? "font-inter font-medium text-text-b" : "" } hover:text-dark-b transition-colors duration-200 ease-in-out`}
+            onClick={() => {NavBarBtnClick(Page)}}>
+            {Page == Pages.HealthInfo ? "Health info" : GetButtonName(Page)}
+          </button>
+
+          <div className={`rounded w-18 h-1 bg-dark-b ${currentPage === Page ? "" : "invisible"}`}/>
+        </div>
+      </>
+
+    )
+  }
+
   return (
     <div className="w-full bg-white">
       <div className="max-w-screen-2xl m-auto h-20 flex justify-around text-base text-grey-t font-intertight">
-        <div className="w-24 m-auto text-center">
-          <button
-            className={`${
-              currentPage === Pages.Home
-                ? "font-inter font-medium text-text-b"
-                : ""
-            } hover:text-dark-b transition-colors duration-200 ease-in-out`}
-            onClick={() => {
-              props.setCurrentPage(Pages.Home);
-              setCurrentPage(Pages.Home);
-            }}
-          >
-            Home
-          </button>
-          <div
-            className={`rounded w-18 h-1 bg-dark-b ${
-              currentPage === Pages.Home ? "" : "invisible"
-            }`}
-          />
-        </div>
-        <div className="w-24 m-auto text-center">
-          <button
-            className={`${
-              currentPage === Pages.Medals
-                ? "font-inter font-medium text-text-b"
-                : ""
-            } hover:text-dark-b transition-colors duration-200 ease-in-out`}
-            onClick={() => {
-              props.setCurrentPage(Pages.Medals);
-              setCurrentPage(Pages.Medals);
-            }}
-          >
-            Medals
-          </button>
-          <div
-            className={`rounded w-18 h-1 bg-dark-b ${
-              currentPage === Pages.Medals ? "" : "invisible"
-            }`}
-          />
-        </div>
-        <div className="w-24 m-auto text-center">
-          <button
-            className={`${
-              currentPage === Pages.HealthInfo
-                ? "font-inter font-medium text-text-b"
-                : ""
-            } hover:text-dark-b transition-colors duration-200 ease-in-out`}
-            onClick={() => {
-              props.setCurrentPage(Pages.HealthInfo);
-              setCurrentPage(Pages.HealthInfo);
-            }}
-          >
-            Health info
-          </button>
-          <div
-            className={`rounded w-18 h-1 bg-dark-b ${
-              currentPage === Pages.HealthInfo ? "" : "invisible"
-            }`}
-          />
-        </div>
-        <div className="w-24 m-auto text-center">
-          <button
-            className={`${
-              currentPage === Pages.Profile
-                ? "font-inter font-medium text-text-b"
-                : ""
-            } hover:text-dark-b transition-colors duration-200 ease-in-out`}
-            onClick={() => {
-              props.setCurrentPage(Pages.Profile);
-              setCurrentPage(Pages.Profile);
-            }}
-          >
-            Profile
-          </button>
-          <div
-            className={`rounded w-18 h-1 bg-dark-b ${
-              currentPage === Pages.Profile ? "" : "invisible"
-            }`}
-          />
-        </div>
-        <div className="w-96"></div>
-
-        <div className="w-24 m-auto text-center">
-          <button
-            className={`${
-              currentPage === Pages.About
-                ? "font-inter font-medium text-text-b"
-                : ""
-            } hover:text-dark-b transition-colors duration-200 ease-in-out`}
-            onClick={() => {
-              props.setCurrentPage(Pages.About);
-              setCurrentPage(Pages.About);
-            }}
-          >
-            About
-          </button>
-          <div
-            className={`rounded w-18 h-1 bg-dark-b ${
-              currentPage === Pages.About ? "" : "invisible"
-            }`}
-          />
-        </div>
+        {NavBarBtn(Pages.Home)}
+        {NavBarBtn(Pages.Medals)}
+        {NavBarBtn(Pages.HealthInfo)}
+        {NavBarBtn(Pages.Profile)}
+        <div className="w-64"></div>
+        {NavBarBtn(Pages.About)}
       </div>
     </div>
   );
